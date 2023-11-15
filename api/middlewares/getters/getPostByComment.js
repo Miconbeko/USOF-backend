b8dbe62b4da2bd52dcbd4a5d64684d9794ce4c30
Miconbeko <stepanov.m2004@gmail.com>
2023-11-16 00:40:54 +0200

    feat: comment the comments

---
+++ api/middlewares/getters/getPostByComment.js
@@ -0,0 +1,24 @@
+import sequelize from "../../database/db.js";
+import ServerError from "../../errors/ServerError.js";
+import {transactionErrorHandler} from "../../errors/handlers.js";
+import retryError from "../../errors/RetryError.js";
+
+const Op = sequelize.Sequelize.Op
+const models = sequelize.models
+
+export default function getPostByComment(req, res, next) {
+    sequelize.inTransaction(async transaction => {
+        return await req.comment.getPost({ transaction })
+    })
+        .then(post => {
+            if (!post)
+                return next(new ServerError(`Post not found`, 404))
+
+            req.post = post
+            req.body.ownerId = post.userId
+            next()
+        })
+        .catch(err => {
+            return transactionErrorHandler(retryError(getPostByComment, err), req, res, next)
+        })
+}
