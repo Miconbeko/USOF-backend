b21d79720a77410516a201afadf69ef6fef38b1c
Miconbeko <stepanov.m2004@gmail.com>
2023-11-16 00:00:30 +0200

    feat: no comments on locked post

--- api/routes/routes/posts.js
+++ api/routes/routes/posts.js
@@ -10,7 +10,7 @@ import {
 import {validationErrorHandler} from "../../errors/handlers.js";
 import PostsController from "../../controllers/PostsController.js";
 import {getDataFromToken, getPaginationParams, getPostById, getUserByToken} from "../../middlewares/getters.js";
-import {checkAdmin, checkLocked, checkOwner, checkTokenSession} from "../../middlewares/checkers.js";
+import {checkAdmin, checkLocked, checkNotLocked, checkOwner, checkTokenSession} from "../../middlewares/checkers.js";
 import getCategoriesByIds from "../../middlewares/getters/getCategoriesByIds.js";
 
 
@@ -18,9 +18,9 @@ const router = express.Router()
 
 
 router.post(`/`,                postCreationValidator, tokenValidator, validationErrorHandler, getDataFromToken, getUserByToken, checkTokenSession, getCategoriesByIds, PostsController.create)
-router.post(`/:id/comments`,    paramIdValidator, commentCreationValidator, tokenValidator, validationErrorHandler, getDataFromToken, getUserByToken, checkTokenSession, getPostById, PostsController.createComment)
+router.post(`/:id/comments`,    paramIdValidator, commentCreationValidator, tokenValidator, validationErrorHandler, getDataFromToken, getUserByToken, checkTokenSession, getPostById, checkNotLocked, PostsController.createComment)
 
-router.put(`/:id`,              paramIdValidator, postCreationValidator, tokenValidator, validationErrorHandler, getDataFromToken, getUserByToken, checkTokenSession, checkAdmin, getPostById, getCategoriesByIds, PostsController.edit)
+router.put(`/:id`,              paramIdValidator, postCreationValidator, tokenValidator, validationErrorHandler, getDataFromToken, getUserByToken, checkTokenSession, getPostById, checkOwner, checkNotLocked, getCategoriesByIds, PostsController.edit)
 
 router.patch(`/:id/lock`,       paramIdValidator, timerValidator, tokenValidator, validationErrorHandler, getDataFromToken, getUserByToken, checkTokenSession, checkAdmin, getPostById, PostsController.lock)
 router.delete(`/:id/lock`,      paramIdValidator, tokenValidator, validationErrorHandler, getDataFromToken, getUserByToken, checkTokenSession, checkAdmin, getPostById, checkLocked, PostsController.unlock)
